a852772ddd10a2dd62068168d9ca7ac7b1a81544
Guillem Ortega <guillem212@hotmail.es>
2020-10-22 21:30:13 +0200

    Grid + Node Develop for pathfinding

---
+++ Assets/Scripts/AI_Scripts/Grid.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grid : MonoBehaviour
+{
+    public Transform startPos;
+    public LayerMask wallMask;
+    public Vector2 gridWorldSize;
+    public float nodeRadious;
+    public float Distance;
+
+    [Range(0, 1)]
+    public float humanoidSize;
+
+    Node[,] grid;
+    public List<Node> FinalPath;
+
+    float nodeDiameter;
+    int gridSizeX, gridSizeY;
+
+    /// <summary>
+    /// Start is called on the frame when a script is enabled just before
+    /// any of the Update methods is called the first time.
+    /// </summary>
+    void Start(){
+        nodeDiameter = nodeRadious * 2;
+        gridSizeX = Mathf.RoundToInt(gridWorldSize.x / nodeDiameter);
+        gridSizeY = Mathf.RoundToInt(gridWorldSize.y / nodeDiameter);
+
+        CreateGrid();
+    }
+
+    ///<summary>
+    ///Create the grid and the node matrix for the pathfinding.
+    ///</summary>
+    private void CreateGrid(){
+        grid = new Node[gridSizeX, gridSizeY];
+
+        Vector3 bottomLeft = transform.position - Vector3.right * gridWorldSize.x / 2 - Vector3.forward * gridWorldSize.y / 2;
+
+        for (int y = 0; y < gridSizeY; y++){
+            for (int x = 0; x < gridSizeX; x++){
+                Vector3 worldPoint = bottomLeft + Vector3.right * (x * nodeDiameter + nodeRadious) + Vector3.forward * (y * nodeDiameter + nodeRadious);
+                bool wall = true;
+
+                if (Physics.CheckSphere(worldPoint, nodeRadious + humanoidSize, wallMask)){
+                    wall = false;
+                }
+                grid[x, y] = new Node(wall, worldPoint, x, y);
+            }
+        }
+    }
+
+    private void OnDrawGizmos() {
+        Gizmos.DrawWireCube(transform.position, new Vector3(gridWorldSize.x, 0, gridWorldSize.y));
+
+        if (grid != null){
+            foreach (Node node in grid){
+                if (node.isWall){
+                    Gizmos.color = Color.white;
+                }
+                else {
+                    Gizmos.color = Color.yellow;
+                }
+
+                if (FinalPath != null){
+                    Gizmos.color = Color.red;
+                }
+
+                Gizmos.DrawWireCube(node.position, Vector3.one * (nodeDiameter - Distance));
+            }
+        }
+    }
+}
